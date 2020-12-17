cab3e3b74913f5a745ac1acfa92f7e63f0f68dad
Marconi <marconieng@gmail.com>
2020-12-17 12:05:47 -0300

    Adiciona exercicio 10

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_1/exercicio10.js
@@ -0,0 +1,7 @@
+let valorDeVenda = 250;
+let custoDoProduto = 200;
+let CustoDoProdutoComImposto = (custoDoProduto  + custoDoProduto * 0.2);
+
+let lucro = valorDeVenda - CustoDoProdutoComImposto;
+
+console.log(lucro);
