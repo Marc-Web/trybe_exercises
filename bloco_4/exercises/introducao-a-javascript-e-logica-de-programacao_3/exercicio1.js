9b2f98aa976803f043b753f03ebaac171f41fc22
Marconi <marconieng@gmail.com>
2020-12-19 10:45:31 -0300

    Adiciona exercicio 1

---
+++ bloco_4/exercises/introducao-a-javascript-e-logica-de-programacao_3/exercicio1.js
@@ -0,0 +1,11 @@
+let n = 5;
+let caractere = '*';
+let linhas = '';
+
+for (let index = 0; index < n; index += 1) {
+  linhas += caractere
+};
+
+for (let index = 0; index < n; index += 1) {
+  console.log(linhas);
+};
